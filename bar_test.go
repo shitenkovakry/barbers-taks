02c370624b924a249baf67fde7701e89a8e1e11d
kry shitenkova <shitenkovakris@mail.ru>
2023-06-21 16:32:18 +0300

    add case2 for test

--- bar_test.go
+++ bar_test.go
@@ -16,3 +16,14 @@ func Test_Case1(t *testing.T) {
 	assert.Equal(t, expectedEnoughBarbers, actualEnoughBarbers)
 	assert.Equal(t, expectedReason, actualReason)
 }
+
+func Test_Case2(t *testing.T) {
+	countOfMenInCity := 9000
+	countOfBarbersInBarbershop := 5
+
+	expectedEnoughBarbers, expectedReason := false, "не хвататет барберов: 52"
+	actualEnoughBarbers, actualReason := CalculateIfThereAreEnoughBarbers(countOfBarbersInBarbershop, countOfMenInCity)
+
+	assert.Equal(t, expectedEnoughBarbers, actualEnoughBarbers)
+	assert.Equal(t, expectedReason, actualReason)
+}
